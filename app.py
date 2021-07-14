4e70d86d62229c7fbaa1f514aa6ed4f9936ba7ea
Abdul Khaliq Abdul-Azeez <abdulkhailqabdulazeez@gmail.com>
2021-07-14 18:32:06 +0000

    mash

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template
+from flask import request
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
+ 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route("/results", methods = ["GET","POST"])
+def results():
+    user_choice_house = request.form["house"]
+    user_choice_car = request.form["car"]
+    user_choice_career = request.form["career"]
+    user_choice_city = request.form["city"]
+    return render_template("results.html", house = user_choice_house, car = user_choice_car, career = user_choice_career, city = user_choice_city)
